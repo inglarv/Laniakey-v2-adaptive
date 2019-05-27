235609c3b53af228e6f8e88c6fcd0c398d951175
inglarv <49346289+inglarv@users.noreply.github.com>
2019-05-27 19:04:59 +0300

    Add files via upload

---
+++ src/config/Ascii.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2018, The DeroGold Association
+// Copyright (c) 2019, The Excelsior Developers
+//
+// Please see the included LICENSE file for more information
+
+#pragma once
+
+const std::string asciiArt =
+"  \n"
+"  \n"
+"  \n"
+"  \n"                                
+"  \n";
+
