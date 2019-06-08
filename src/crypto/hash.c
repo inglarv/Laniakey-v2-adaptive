d30e90e8df58e6f53d9b13b9b9167f2bcf34bcc3
inglarv <49346289+inglarv@users.noreply.github.com>
2019-06-08 13:52:39 +0300

    Update hash.c

--- src/crypto/hash.c
+++ src/crypto/hash.c
@@ -1,7 +1,7 @@
 // Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
 //
 // This file is part of Bytecoin.
-//
+// 
 // Bytecoin is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
